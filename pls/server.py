b813ab5eb76d5950051893a3b2e94b82ff1439da
MartimVideira <mitram790@gmail.com>
2025-04-09 18:24:07 +0100

    Early version of go to definition working on predicates

--- pls/server.py
+++ pls/server.py
@@ -24,23 +24,30 @@ def node_to_range(node:Node):
 class Term:
     def __init__(self,name):
         self.name : str = name
+        self.arity = 0
+    def key(self)-> str:
+        return f"{self.name}/{self.arity}"
 
 class Predicate(Term):
     def __init__(self,name,arity):
         super().__init__(name)
         self.arity : int = arity
-        self.definitions: list[types.Position] = []
+        self.definitions: list[types.Range] = []
+        self.uri = ""
 
 
 class Functor(Term):
     def __init__(self,name,args):
         super().__init__(name)
         self.args: list= args
+        self.arity = len(args)
+
 
 class Operator(Term):
     def __init__(self,operator:str,operands:list[Term]):
         super().__init__(operator)
         self.operands = operands
+        self.arity = len(operands)
 
 
 class PLS(LanguageServer):
@@ -49,9 +56,11 @@ class PLS(LanguageServer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.diagnostics = {}
-        self.visit_map  = {}
-        self.predicates = []
+        self.predicate_index = {}
         self.trees = {}
+        self.visit_map  = {}
+        self.current_uri = ""
+        self.build_visitors()
 
     def add_visit(self,node_type:str,f):
         self.visit_map[node_type] = f
@@ -61,10 +70,22 @@ class PLS(LanguageServer):
             return self.visit_map[node.type](node)
         raise TypeError(f"There is no registered visitor for: {node.type}\n{node}")
 
+    def build_visitors(self):
+
+        self.add_visit('clause_term',self.visit_clause_term)
+        self.add_visit('directive_term',self.visit_directive)
+        self.add_visit('atom',self.visit_atom)
+        self.add_visit('functional_notation',self.visit_functional_notation)
+        self.add_visit('operator_notation',self.visit_operator_notation)
+        self.add_visit('integer',self.text_visit)
+        self.add_visit('variable_term',self.visit_variable_term)
+    
     def parse(self, document: TextDocument):
 
-        self.predicates = []
+        self.predicate_index = {}
+        self.current_uri = document.uri
         tree = self._parse(document.source)
+
         diagnostics = self.visit_errors(tree)
         self.diagnostics[document.uri] = (document.version, diagnostics)
         self.trees[document.uri] = tree
@@ -122,7 +143,7 @@ class PLS(LanguageServer):
             case [atom,_,arg_list,_]:
                 name = self.visit_atom(atom)
                 args = self.visit_arg_list(arg_list)
-                return Functor(name,args)
+                return Functor(name.name,args)
             case x:
                 raise TypeError(f"Invalid shape of argument list: {x}")
         return
@@ -133,8 +154,6 @@ class PLS(LanguageServer):
 
     def visit_operator_notation(self,node:Node):
         assert node.type == 'operator_notation'
-        print(node)
-        print(node.children)    
         match node.children:
             case [head,op,body]:
                 head = self.visit(head)
@@ -150,11 +169,16 @@ class PLS(LanguageServer):
         if type(f) == Functor:
             arity = len(f.args)
         predicate = Predicate(f.name,arity)
-        predicate.definitions.append(parent.start_point)
-        self.predicates.append(predicate)
+        predicate.definitions.append(node_to_range(parent))
+        predicate.uri = self.current_uri
+        key = predicate.key()
+        if key in self.predicate_index:
+            # TODO: check definition location
+            self.predicate_index[key].append(predicate)
+        else:
+            self.predicate_index[key] = [predicate]
         return
     def visit_directive(self,node:Node):
-        print(node)
         return
     def _parse(self, doc:str):
 
@@ -172,10 +196,30 @@ class PLS(LanguageServer):
             self.visit(child)
 
         return tree
+    def go_to_definition(self,tree,position:types.Position):
+        logging.debug(position)
+        node = node_at_position(tree.root_node,position) 
+        if node is None:
+            logging.debug("Didn't found any node")
+            return None
+        print(position)
+        print("Node: ",node)
+        print("Node: ",node.text)
+        print(node.start_point,node.end_point)
+        if node.parent.type == "functional_notation" and node.parent.children[0] == node:
+            functor = self.visit(node.parent)
+            predicates = self.search(functor)
+            if predicates is None:
+                return None
+
+            return types.Location(uri=predicates[0].uri,range=predicates[0].definitions[0])
+    def search(self,functor:Functor):
+        return self.predicate_index.get(functor.key())
 
 
 server = PLS("diagnostic-server", "v1")
 
+print = logging.debug
 
 @server.feature(types.TEXT_DOCUMENT_DID_OPEN)
 def did_open(ls: PLS, params: types.DidOpenTextDocumentParams):
@@ -210,46 +254,29 @@ def did_change(ls: PLS, params: types.DidOpenTextDocumentParams):
         #     )
         # )
 
-@server.feature(types.TEXT_DOCUMENT_TYPE_DEFINITION)
-def goto_type_definition(ls:PLS, params: types.TypeDefinitionParams):
-    """Jump to an object's type definition."""
-    doc = ls.workspace.get_text_document(params.text_document.uri)
-    index = ls.index.get(doc.uri)
-    if index is None:
-        return
 
-    try:
-        line = doc.lines[params.position.line]
-    except IndexError:
-        line = ""
-
-    word = doc.word_at_position(params.position)
-
-    # for match in ARGUMENT.finditer(line):
-    #     if match.group("name") == word:
-    #         if (range_ := index["types"].get(match.group("type"), None)) is not None:
-    #             return types.Location(uri=doc.uri, range=range_)
-
-
-@server.feature(types.TEXT_DOCUMENT_DEFINITION)
+@server.feature('textDocument/definition')
 def goto_definition(ls:PLS, params: types.DefinitionParams):
     """Jump to an object's definition."""
+    logging.log(logging.DEBUG,"Hellooooo\n\n\n")
     doc = ls.workspace.get_text_document(params.text_document.uri)
     tree = ls.trees.get(doc.uri)
+    logging.log(logging.DEBUG,str(params.position))
     if tree is None:
-        return
+        return []
+    result = ls.go_to_definition(tree,params.position)
+    logging.log(logging.DEBUG,result)
+    return result
 
 
-    word = doc.word_at_position(params.position)
-
-    # Is word a type?
-    # if (range_ := index["types"].get(word, None)) is not None:
-    #     return types.Location(uri=doc.uri, range=range_)
 
 def node_at_position(node:Node,p : types.Position):
-    after_start = (node.start_point.column <= p.character and node.start_point.row <= p.line) 
-    before_end = (node.end_point.column >= p.character and node.end_point.row >= p.line)
-    contained = after_start and before_end
+    in_row_between = node.start_point.row < p.line and node.end_point.row > p.line
+    in_first_line = node.start_point.row == p.line and node.start_point.column <= p.character
+    in_last_line = node.end_point.row == p.line and node.end_point.column >= p.character
+    if (node.start_point.row == node.end_point.row and node.start_point.row== p.line) and not (node.start_point.column <= p.character and node.end_point.column >= p.character):
+        return None
+    contained = in_row_between or in_first_line or in_last_line
     if not contained:
         return None
     current = node
@@ -260,32 +287,6 @@ def node_at_position(node:Node,p : types.Position):
     return current
     
 
-
-@server.feature(types.TEXT_DOCUMENT_DECLARATION)
-def goto_declaration(ls:PLS, params: types.DeclarationParams):
-    """Jump to an object's declaration."""
-    doc = ls.workspace.get_text_document(params.text_document.uri)
-    index = ls.index.get(doc.uri)
-    if index is None:
-        return
-
-    try:
-        line = doc.lines[params.position.line]
-    except IndexError:
-        line = ""
-
-    word = doc.word_at_position(params.position)
-
-    # for match in ARGUMENT.finditer(line):
-    #     if match.group("name") == word:
-    #         linum = params.position.line
-    #         return types.Location(
-    #             uri=doc.uri,
-    #             range=types.Range(
-    #                 start=types.Position(line=linum, character=match.start()),
-    #                 end=types.Position(line=linum, character=match.end()),
-    #             ),
-    #         )
 
 
 @server.feature(
@@ -303,15 +304,21 @@ def completions(params: types.CompletionParams):
         types.CompletionItem(label="prolog"),
         types.CompletionItem(label="world"),
         types.CompletionItem(label="friend"),
+        types.CompletionItem(label="Elsa"),
     ]
 
 def main():
-    logging.basicConfig(filename='pygls.log', filemode='w', level=logging.DEBUG)
+    logging.basicConfig(filename='/home/martim/Desktop/pls/pygls.log', filemode='w', level=logging.DEBUG)
     server.start_io()
-if __name__ == "__main__":
-    # main()
+def debug():
     s = open("teste.pl").read()
     t : Tree = parser.parse(bytes(s,"utf-8"))
     print(t.root_node)
     server._parse(s)
-    
+    server.go_to_definition(t,types.Position(character=19,line=3))
+
+if __name__ == "__main__":
+    if True:
+        main()
+    else:
+        debug()
