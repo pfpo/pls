495c3b70cc460a44c5e9efdbedbe6a98d8880dd8
MartimVideira <mitram790@gmail.com>
2025-05-12 18:05:57 +0100

    Some test changes

--- examples/clpfd.pl
+++ examples/clpfd.pl
@@ -1,9 +1,9 @@
-positive_integer(N) :- N amsdk 1.
+positive_integer(N) :- askdf amsdk 1.
 
 % Acho que não aceita o comentario da linha 8
 % Build The Domain of Ms where each job must be performed by a worker that can perform that job
 build_domain([],_):-!.
 build_domain([M|MS],[P|PeopleJob]):-
     list_to_fdset(P,SetP),
-   % M in_set SetP,
+    M in_set SetP,
     build_domain(MS,PeopleJob).
