dca9ed774eb8652e4795b6382d4b7c61b1dbbc18
MartimVideira <mitram790@gmail.com>
2025-05-20 21:47:56 +0100

    Implemented Unused Variable warnings

---
+++ pls/passes/unused_variable.py
@@ -0,0 +1,36 @@
+from tree_sitter import Node, Parser, Language
+from pls.model import Term, Functor, Predicate, Variable, Scope,SymbolTable
+from pls.utils import node_to_range, node_and_parent_with_text
+from pls.tree_visitor import TreeVisitor
+from pls.annotations import Annotations
+from lsprotocol import types
+
+
+class UnusedVariablePass(TreeVisitor):
+    def __init__(self,table: SymbolTable):
+        super().__init__()
+        self.table = table
+        self.reports = []
+
+
+    def start(self, node: Node):
+        self.visit_all_children(node)
+
+    def build_visitors(self):
+        self.add_visit("variable_term", self.visit_variable_term)
+        self.set_default_visitor(self.visit_all_children)
+
+    def visit_variable_term(self, node: Node):
+        variable = self.table.notes[node]
+        if variable is None or type(variable) is not Variable:
+            return 
+        if len(variable.references) == 1:
+            severity = types.DiagnosticSeverity.Warning
+            message = "Unused Variable"
+            report =  types.Diagnostic(
+                message=message + variable.name,
+                severity=severity,
+                range=node_to_range(node),
+            )
+            self.reports.append(report)
+
