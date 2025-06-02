7f90193a74b0717c53e78c09e6ee4f11226f83d4
MartimVideira <mitram790@gmail.com>
2025-06-02 18:35:33 +0100

    Disabled hover information returning node ast information

--- examples/b/l.pl
+++ examples/b/l.pl
@@ -1,29 +1,14 @@
-:- module(sdfskdmfskl,[yas/7,l/7]).
 
 :- consult('utils.pl').
 
 :- use_module(library(clpfd)).
 
 % Solve Shift Problem Minimization using cumulatives 
-% +TimeoutOptions time_out(Timeout,Flag) 
-% -MS dvar with the scheduling
-% -Amount the amount of workers used
-% shift_problem(+TimeoutOptions,-MS,-Amount,+NWorkers,+Jobs,+WorkersPerJob,+MinNumWorkers):-
-
-
-%!  l(TimeoutOptions,MS,Amount,Arg0,Arg1,WorkersPerJob,MinNumWorkers)
-%
-% Predicate Description 
-%
-% @param TimeoutOptions  Argument's TimeoutOptions description
-% @param MS  Argument's MS description
-% @param Amount  Argument's Amount description
-% @param Arg0  Argument's Arg0 description
-% @param Arg1  Argument's Arg1 description
-% @param WorkersPerJob  Argument's WorkersPerJob description
-% @param MinNumWorkers  Argument's MinNumWorkers description
-
-yas(TimeoutOptions,MS,Amount,[],1,WorkersPerJob,MinNumWorkers):-
+% - `+TimeoutOptions` time_out(Timeout,Flag) 
+% - `-MS` dvar with the scheduling
+% - `Amount` the amount of workers used
+% `shift_problem(+TimeoutOptions,-MS,-Amount,+NWorkers,+Jobs,+WorkersPerJob,+MinNumWorkers)`
+shift_problem(TimeoutOptions,MS,Amount,[],1,WorkersPerJob,MinNumWorkers):-
     length(Jobs, N),
     length(MS, N),
     build_domain(MS,WorkersPerJob),
@@ -35,5 +20,4 @@ yas(TimeoutOptions,MS,Amount,[],1,WorkersPerJob,MinNumWorkers):-
     \+ nvalue(Amount,MS),    
     print('.'),
     Amount >= MinNumWorkers,
-    labeling([minimize(Amount),ff,TimeoutOptions],MS).
-
+    labeling([minimize(Amount),ff,TimeoutOptions],MS),shift_problem(0,1,2,3,4,5,6).
