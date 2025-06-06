6ba74277f31fdd708d2ace59473b08095a4efab6
MartimVideira <mitram790@gmail.com>
2025-06-06 12:39:30 +0100

    Storing information of binary operators

--- pls/prolog_visitor.py
+++ pls/prolog_visitor.py
@@ -194,6 +194,8 @@ class PrologVisitor(TreeVisitor):
                 operand = self.visit(operand, opts)
                 return operand
             case [head, op, body]:
+                predicate = self.get_predicate(Functor(bytes.decode(op.text),[None,None]))
+                predicate.add_reference(self.uri,op)
                 is_parameter_definition = self.is_parameter_start_point(opts, op)
                 if is_parameter_definition:
                     opts = self.set_parameter_definition(opts)
