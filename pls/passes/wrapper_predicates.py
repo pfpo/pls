45d20ce543d00cbc170866e566e1e90db656dea2
Pedro <pedro.oliveira2003g@gmail.com>
2026-03-30 21:45:00 +0100

    feat: wrapper predicate code action

--- pls/passes/wrapper_predicates.py
+++ pls/passes/wrapper_predicates.py
@@ -1,6 +1,6 @@
 from tree_sitter import Node, QueryCursor
 from lsprotocol import types
-from pls.utils import node_to_range, RangedAction
+from pls.utils import node_to_range, contained_range, RangedAction
 from .analyser import Analyser, PrologAnalyseable
 
 class WrapperPredicatesAppendsAnalysis(Analyser):
@@ -19,10 +19,11 @@ class WrapperPredicatesAppendsAnalysis(Analyser):
 
 
         for index, m in enumerate(self.matches):
-            (_, match) = m
-            wrapper_predicate = match["wrapper"][0]
-            self.add_wrapper_predicate_warning(wrapper_predicate)
-            self.add_wrapper_predicate_code_action(wrapper_predicate, index)
+            if self.check_wrapper_predicate(index):
+                (_, match) = m
+                wrapper_predicate = match["wrapper"][0]
+                self.add_wrapper_predicate_warning(wrapper_predicate)
+                self.add_wrapper_predicate_code_action(wrapper_predicate, index)
 
     def add_wrapper_predicate_warning(self, node: Node):
         range = node_to_range(node)
@@ -39,7 +40,58 @@ class WrapperPredicatesAppendsAnalysis(Analyser):
         range = node_to_range(node)
         (_, match) = self.matches[index]
 
-        calling_arg = match["calling_arg"][0]
-        called_arg = match["called_arg"][0]
+        title = "Remove wrapper predicate and replace calls"
+        changes = {}
 
-        return
+        calling = match["calling"][0]    
+        called = match["called"][0]
+
+        calling_predicate = self.table.notes[calling]
+        called_predicate = self.table.notes[called]
+
+        locations = calling_predicate.name_references
+
+        for loc in locations:
+            if not contained_range(range, loc.range):
+                if loc.uri not in changes:
+                    changes[loc.uri] = []
+                changes[loc.uri].append(types.TextEdit(
+                    range=loc.range,
+                    new_text=called_predicate.name
+                ))
+        
+        changes[self.uri] = changes.get(self.uri, []) + [
+            types.TextEdit(
+                range=range,
+                new_text=""
+            )
+        ]
+
+        action = types.CodeAction(
+            title=title,
+            kind=types.CodeActionKind.QuickFix,
+            edit=types.WorkspaceEdit(changes=changes)
+        )
+
+        self.add_file_action(RangedAction(action, range))
+
+    def check_wrapper_predicate(self, index: int) -> bool:
+        (_, match) = self.matches[index]
+
+        calling_arg = match["calling_arg"][0]
+        calling_arg_list = [n for n in calling_arg.children if n.type != "arg_list_separator"]
+        called_arg = match["called_arg"][0]
+        called_arg_list = [n for n in called_arg.children if n.type != "arg_list_separator"]
+        op = match["op"][0]
+
+        if op.text.decode("utf-8") != ":-":
+            return False
+
+        if len(calling_arg_list) != len(called_arg_list):
+            return False
+        
+        for c_arg, d_arg in zip(calling_arg_list, called_arg_list):
+            if c_arg.type != d_arg.type or c_arg.text != d_arg.text:
+                return False
+
+        return True
--- pls/queries/wrapper_predicate.scm
+++ pls/queries/wrapper_predicate.scm
@@ -4,11 +4,11 @@
     (functional_notation
       (arg_list) @calling_arg
     ) @calling
-    (binary_operator)
+    (binary_operator) @op
     (functional_notation
       (arg_list) @called_arg
     ) @called
-  ) @op
+  )
   .
   (end)
 ) @wrapper
